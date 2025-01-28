11c56b7d9dcdb19c3cbbe5f48fe98e4c91ede27a
AlexandreCo <alexco@alexco.com>
2025-01-28 17:25:17 +0100

    display log

--- README.md
+++ README.md
@@ -1,26 +1,49 @@
 # logger
-## install pip
+
+## install pip
+
 ```
 sudo apt update
 sudo apt-get install python3-pip
 ```
-## install freebox-api
+
+## install freebox-api
+
 ```
 python3 -m venv venv/
 venv/bin/pip install freebox-api
 ```
-## clone freebox-logger
+
+## clone freebox-logger
+
 ```
 mkdir freebox-api
 cd freebox-api
 git clone git@github.com:AlexandreCo/logger.git tests/
 ```
-## crontab freebox-logger every 5min
+
+## crontab freebox-logger every 5min
+
 ```
 crontab -e
-```
 write
-```
 */5 * * * * /home/freebox/venv/bin/python /home/freebox/freebox-api/tests/freebox_log_host.py
 ```
 
+## install flask
+
+```
+~/venv/bin/pip install flask
+```
+
+## install matplotlib
+
+```
+~/venv/bin/pip install matplotlib
+```
+
+## Run flask
+
+```
+/home/freebox/venv/bin/flask --app '/home/freebox/freebox-api/tests/log_server.py' run --host=0.0.0.0
+```
---
+++ flask/log_server.py
@@ -0,0 +1,109 @@
+import sqlite3
+from flask import Flask, render_template, request
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+
+    totime = request.args.get('to')
+    if totime == None:
+        to_time = int(datetime.timestamp(datetime.now()))
+    else :
+        to_time = int(totime)
+
+    fromtime = request.args.get('from')
+    if fromtime == None:
+        # last 24 hours
+        from_time=to_time-24*60*60
+    else:
+        from_time = int(fromtime)
+
+    get_jpg_file(from_time,to_time)
+    return render_template('index.html')
+
+@app.route('/data', methods=['GET'])
+def data():
+    host = request.args.get('host')
+    if host == None:
+        return "please give host"
+
+    totime = request.args.get('to')
+    if totime == None:
+        to_time = int(datetime.timestamp(datetime.now()))
+    else :
+        to_time = int(totime)
+
+    fromtime = request.args.get('from')
+    if fromtime == None:
+        from_time=to_time-24*60*60
+    else:
+        from_time = int(fromtime)
+
+    print(host,from_time,to_time)
+    
+    html = "host : " + host + "<br>" + str(datetime.fromtimestamp(int(from_time))) + "<br>"  + str (datetime.fromtimestamp(int(to_time))) + "<br>"
+
+    # last 24 hours
+    # to_time=int(datetime.timestamp(datetime.now()))
+    # from_time=to_time-24*60*60
+    rows = get_data(host,from_time,to_time)
+    html+="<table class=\"table\">"
+    html+="<tr><th>Timestamp</th><th>Device id</th><th>Name</th><th>type</th><th>First activity</th><th>Last activity</th><th>Last time reachable</th>"
+    for row in rows:
+        html += "<tr><td>"+str(datetime.fromtimestamp(int(row[1])))+"</td><td>"+row[2]+"</td><td>"+row[3]+"</td><td>"+row[4]+"</td><td>"+str(datetime.fromtimestamp(int(row[5])))+"</td><td>"+str(datetime.fromtimestamp(int(row[6])))+"</td><td>"+str(datetime.fromtimestamp(int(row[7])))+"</td></tr>"
+    html+="</table><style>table, th, td {  border: 1px solid black;  border-collapse: collapse;}</style>"
+    return html
+
+database='/home/freebox/hosts.db'
+def generate_graph(plt,host,value,from_time,to_time):
+    rows = get_data(host,from_time,to_time)
+    last_activity = []
+    for row in rows:
+        last_activity.append(datetime.fromtimestamp(int(row[6])))
+
+    #last_activity
+    values = [value for ts in last_activity]
+    plt.plot(last_activity, values, 'o', label=host)
+    plt.legend()
+
+def get_data(host,from_time,to_time):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    sql='SELECT * FROM devices where primary_name is "' + host + '" and timestamp between ' + str(from_time) + ' and ' + str(to_time)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def getHosts(from_time,to_time):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    sql="SELECT DISTINCT primary_name FROM devices where timestamp between " + str(from_time) + " and " + str(to_time)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def get_jpg_file(from_time,to_time):
+    plt.cla()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y %H:%M:%S'))
+    plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=5))
+    hosts=getHosts(from_time,to_time)
+    value = 1
+    for host in hosts:
+        generate_graph(plt, host[0], value,from_time,to_time)
+        value += 1
+    plt.gcf().autofmt_xdate()
+    plt.savefig('static/images/lastday.png')
+
+
+if __name__ == '__main__':
+    # last 24 hours
+    to_time=int(datetime.timestamp(datetime.now()))
+    from_time=to_time-24*60*60
+    get_jpg_file(from_time,to_time)
+
